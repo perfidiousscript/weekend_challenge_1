807a700b3f3d044a1c90b15dbf57082f038b7759
perfidiousscript <sam.moss321@gmail.com>
2015-10-18 15:17:10 -0500

    appends to DOM now

--- app.js
+++ app.js
@@ -19,13 +19,28 @@ $(document).ready(function(){
 	 		//converts each form field into a key with that name
 	 		info[field.name] = field.value;
 	 	});
+	 	//check that we are getting a nice object from the stripped data:
+	 	//console.log(info);
 
-	 	console.log(info);
+	 	//takes the object with the emplyee data in it, pushes it to the
+	 	//global employeeArray var then calls appendToDom with the values object
+	 	//thus placing the info onto the DOM
+	 	$("#employeeInfo").find("input[type=text]").val();
+	 	employeeArray.push(values);
+	 	appendToDom(values);
 
 	 });
 })
 
 //this function will append whatever is passed into it into the DOM
 function appendToDom(){
+	$("employeeInfo").append("<div class='employee'></div>")
 
+	//make the location a local varible
+	var $el = $("#employeeContainer").children().last();
+
+	$el.append("<p>" + employee.employeename + "</p>");
+	$el.append("<p>" + employee.employeenumber + "</p>");
+	$el.append("<p>" + employee.jobTitle + "</p>");
+	$el.append("<p>" + employee.YearlySalary + "</p>");
 }
